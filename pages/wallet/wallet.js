869021e11b3f617a652148c1028cd5fb73780d83
ss <2065514544@qq.com>
2019-02-16 23:36:36 +0800

    我的钱包界面修改
    我的钱包

--- pages/wallet/wallet.js
+++ pages/wallet/wallet.js
@@ -1,121 +1,65 @@
-var app = getApp();
+//index.js
+//获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     wallet: {
-      money: '',
-      imagePath: [
-        "../image/Dollar.svg",
-        "../image/card.svg",
-        "../image/right.svg"
-      ]
-    },
-    walletList: [{
-        name: "我的银行卡",
-        imagePath: "../image/right.svg",
-        add: "未添加",
-        id: 1
-      },
-      {
-        name: "微信支付",
-        imagePath: "../image/right.svg",
-        add: "已添加",
-        id: 2
-      },
-      {
-        name: "第三方支付",
-        imagePath: "../image/right.svg",
-        add: "已添加",
-        id: 3
+      money: "8000.00",
+    }
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
       }
-    ]
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    this.getWallet()
   },
-  /**
-   * 跳转绑定银行卡
-   */
-  ToBindbankcards: function() {
-    wx: wx.navigateTo({
-      url: '../bindbankcards/bindbankcards',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   },
-  recharge: function() {
+  recharge: function(e) {
     wx.navigateTo({
-      url: '../recharge/recharge',
+      url: '/pages/OnlineRecharge/OnlineRecharge',
     })
   },
-  cash: function() {
-    wx.navigateTo({
-      url: '../cash/cash',
-    })
-  },
-  account: function() {
-    wx.navigateTo({
-      url: '../account/account',
-    })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-          var that = this;
-          that.getWallet();
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  },
-
   getWallet: function() {
     var that = this;
     wx.request({
@@ -129,7 +73,7 @@ Page({
         var wallet = that.data.wallet;
         var money = res.data.data.hcWallet.balance;
         wallet.money = money;
-        if (money==null){
+        if (money == null) {
           wallet.money = 0;
         }
         that.setData({
@@ -138,5 +82,5 @@ Page({
         });
       }
     })
-  },
+  }
 })
