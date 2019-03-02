ef83472a80f16f26f81132106538eb0b7539da50
ss <2065514544@qq.com>
2019-03-02 19:40:57 +0800

    扫码提示
    扫码提示

--- pages/account/account.js
+++ pages/account/account.js
@@ -1,10 +1,13 @@
+//获取应用实例
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    sourceType: ['充值', '订单消费', '订单返回消费', '提现','佣金'], //充值类型
   },
 
   /**
@@ -15,51 +18,22 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
     
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  accountDetails: function () {
+    var that = this;
+    wx.request({
+      url: app.globalData.url + '/api/wallet/getWallet?sid=' + app.globalData.sid + '&userId=' + app.globalData.uid,
+      method: "POST",
+      header: {
+        'X-Requested-With': 'APP'
+      },
+      success: function (res) {
     
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
